9e3c7cf0adb996d762f86141a3c48ee6b8684889
Louis Alary <louis.alary34@gmail.com>
2022-12-16 17:49:54 +0100

    User tests

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client
 
-# Create your tests here.
+class UserModelTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='user', email='user@user.com', password='root')
+        self.assertEqual(user.email, 'user@user.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_authentication_successful(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='user', email='user@user.com', password='root')
+        c = Client()
+        islogged = c.login(username='user', password='root')
+        self.assertEqual(islogged, True)
+
+    def test_authentication_failed(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='user', email='user@user.com', password='root')
+        c = Client()
+        islogged = c.login(username='user', password='password')
+        self.assertEqual(islogged, False)
+
+    def test_user_new_adress(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='user', email='user@user.com', password='root')
