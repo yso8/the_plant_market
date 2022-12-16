151802be97dfda1fbbf1436391db500506fd039d
Louis Alary <louis.alary34@gmail.com>
2022-12-16 17:20:47 +0100

    Delivery and Category tables

--- store/models.py
+++ store/models.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 from shop.settings import AUTH_USER_MODEL
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=128)
     slug = models.SlugField(max_length=128)
@@ -11,6 +15,7 @@ class Product(models.Model):
     quantity = models.IntegerField(default=0)
     description = models.TextField(blank=True)
     picture = models.ImageField(upload_to='img', blank=False, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -19,15 +24,28 @@ class Product(models.Model):
         return reverse("product_detail", kwargs={"slug": self.slug})
 
 
+class Delivery(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    time_days = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name} : {self.price} {self.time_days}"
+
+
 class Order(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     ordered = models.BooleanField(default=False)
     ordered_date = models.DateTimeField(blank=True, null=True)
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.user} : {self.product.name} ({self.quantity})"
+
+    def total(self):
+        return product
 
 
 class Cart(models.Model):
