c9e9c118900c0b270ee1a53eb217ae666d032bb8
Louis Alary <louis.alary34@gmail.com>
2022-12-16 17:18:13 +0100

    Mvc for managing user's addresses

--- account/models.py
+++ account/models.py
@@ -1,10 +1,17 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.formfields import PhoneNumberField
-from django.db import models
-
 
 class Shopper(AbstractUser):
-    city = models.CharField(max_length=38, blank=False),
-    phone_number = PhoneNumberField(region="FR"),
-    postal_code = models.CharField(max_length=5),
+    pass
+
+
+class Address(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=38, default='default')
+    postal_code = models.CharField(max_length=5)
     address_complement = models.CharField(max_length=50, blank=True)
+    user = models.ForeignKey(Shopper, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- account/views.py
+++ account/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model, login, logout, authenticate
 from django.shortcuts import render, redirect
 from django.views import generic
+from .models import Address
+from .forms import AddressForm
+from django.http import HttpResponseRedirect
 
 User = get_user_model()
 
@@ -46,7 +49,66 @@ def account_home(request):
 
 
 def account_address(request):
-    return render(request, 'account/user_address.html')
+    # fetchs les adresses du user
+    addresses = Address.objects.filter(user=request.user)
+    return render(request, 'account/user_address.html', context={"addresses": addresses})
+
+
+def account_address_add(request):
+    if request.method == 'POST':
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            # create new address
+            name = form.cleaned_data['name']
+            city = form.cleaned_data['city']
+            postal_code = form.cleaned_data['postal_code']
+            address_complement = form.cleaned_data['address_complement']
+            # check if address name already exists or not
+            address = Address.objects.filter(name=name, user=request.user)
+            if address:
+                error = True
+                return render(request, 'account/user_add_address.html', {'form': form, 'error': error})
+            else:
+                address = Address(name=name, city=city,
+                                  postal_code=postal_code, address_complement=address_complement, user=request.user)
+                address.save()
+                return HttpResponseRedirect('/account/addresses')
+    else:
+        form = AddressForm()
+
+    return render(request, 'account/user_add_address.html', {'form': form})
+
+
+def account_address_update(request, id):
+    if request.method == 'PUT':
+        form = AddressForm(request.PUT)
+        if form.is_valid():
+            # create new address
+            name = form.cleaned_data['name']
+            city = form.cleaned_data['city']
+            postal_code = form.cleaned_data['postal_code']
+            address_complement = form.cleaned_data['address_complement']
+            # check if address name already exists or not
+            address = Address.objects.filter(name=name, user=request.user)
+            if address:
+                error = True
+                return render(request, 'account/user_update_address.html', {'form': form, 'error': error})
+            else:
+                address = Address(name=name, city=city,
+                                  postal_code=postal_code, address_complement=address_complement, user=request.user)
+                address.save()
+                return HttpResponseRedirect('/account/addresses')
+    else:
+        # get the current data of the selected address
+        address = Address.objects.filter(id=id, user=request.user).first()
+        form = AddressForm(initial={'name': address.name, 'city': address.city, 'postal_code': address.postal_code,
+                                    'address_complement:': address.address_complement})
+        return render(request, 'account/user_update_address.html', {'form': form})
+
+
+def account_address_delete(request, id):
+    Address.objects.filter(id=id, user=request.user).delete()
+    return HttpResponseRedirect('/account/addresses')
 
 
 def account_order(request):
@@ -55,13 +117,3 @@ def account_order(request):
 
 def account_return(request):
     return render(request, 'account/user_return.html')
-
-
-def add_new_adress(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        user = User.objects.create_user(username=username, email=email, password=password)
-        login(request, user)
-        return redirect('index')
