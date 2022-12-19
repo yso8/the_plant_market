c63e99ae598e3984741eb88b9336f1d58558a379
Louis Alary <louis.alary34@gmail.com>
2022-12-19 11:02:17 +0100

    Add 404 page

--- shop/urls.py
+++ shop/urls.py
@@ -1,26 +1,30 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from store.views import index, product_detail, add_to_cart, cart, delete_cart, index_add_to_cart, delete_product_to_cart, add_to_favorite, select_delivery_method, payment_method,filter_products, show_favorite
+from store.views import index, product_detail, add_to_cart, cart, delete_cart, index_add_to_cart, \
+    delete_product_to_cart, add_to_favorite, select_delivery_method, payment_method, filter_products, show_favorite
 from account.views import signup, logout_user, login_user
 from shop import settings
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('', include('account.urls')),
-    path('products-filter/<str:name>', filter_products, name="product_filters"),
-    path('admin/', admin.site.urls),
-    path('signup/', signup, name='signup'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name="logout"),
-    path('product/<str:slug>/', product_detail, name='product_detail'),
-    path('product/<str:slug>/add-to-cart/', add_to_cart, name='add-to-cart'),
-    path('favorite/', show_favorite, name="show-favorite"),
-    path('favorite/<int:id>/add', add_to_favorite, name="add-to-favorite"),
-    path('cart/', cart, name="cart"),
-    path('index-add-to-cart/<str:slug>/', index_add_to_cart, name="index-add-to-cart"),
-    path('cart/delete-product/<str:slug>/', delete_product_to_cart, name="delete-product-to-cart"),
-    path('cart/delivery', select_delivery_method, name="address_delivery_selection"),
-    path('payment/', payment_method, name="payment"),
-    path('payment-check/', payment_method, name="payment-check")
+                  path('', index, name='index'),
+                  path('', include('account.urls')),
+                  path('products-filter/<str:filter>', filter_products, name="product_filters"),
+                  path('admin/', admin.site.urls),
+                  path('signup/', signup, name='signup'),
+                  path('login/', login_user, name='login'),
+                  path('logout/', logout_user, name="logout"),
+                  path('product/<str:slug>/', product_detail, name='product_detail'),
+                  path('product/<str:slug>/add-to-cart/', add_to_cart, name='add-to-cart'),
+                  path('favorite/', show_favorite, name="show-favorite"),
+                  path('favorite/<int:id>/add', add_to_favorite, name="add-to-favorite"),
+                  path('cart/', cart, name="cart"),
+                  path('index-add-to-cart/<str:slug>/', index_add_to_cart, name="index-add-to-cart"),
+                  path('cart/delete-product/<str:slug>/', delete_product_to_cart, name="delete-product-to-cart"),
+                  path('cart/delivery', select_delivery_method, name="address_delivery_selection"),
+                  path('payment/', payment_method, name="payment"),
+                  path('payment-check/', payment_method, name="payment-check")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# custom handler for 404 page
+handler404 = 'store.views.handle_not_found'
