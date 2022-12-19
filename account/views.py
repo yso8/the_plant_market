47c3b8d031f71d5f6911a0fd531fc1098955693c
Louis Alary <louis.alary34@gmail.com>
2022-12-19 11:00:34 +0100

    Add verification to check if user exist on signup

--- account/views.py
+++ account/views.py
@@ -1,24 +1,44 @@
 from django.contrib.auth import get_user_model, login, logout, authenticate
 from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Address
+from .models import Address, Shopper
 from .forms import AddressForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-User = get_user_model()
-
 
 def signup(request):
     if request.method == "POST":
+        # get all the fields from the form
         username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
-        user = User.objects.create_user(username=username, email=email, password=password)
-        login(request, user)
-        return redirect('index')
 
-    return render(request, 'account/signup.html')
+        # check if the username and email are already in use
+        check_username = Shopper.objects.filter(username=username)
+        check_email = Shopper.objects.filter(email=email)
+        print(check_email)
+
+        if not check_username and not check_email:
+            user = Shopper.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            return redirect('index')
+        # if username already in use
+        elif check_username and check_email:
+            error_username = True
+            error_email = True
+            return render(request, 'account/signup.html',
+                          {"error_username": error_username, "error_email": error_email})
+        # if email already in use
+        elif check_email:
+            error_email = True
+            return render(request, 'account/signup.html', {"error_email": error_email})
+        else:
+            error_username = True
+            return render(request, 'account/signup.html', {"error_username": error_username})
+
+    else:
+        return render(request, 'account/signup.html')
 
 
 def login_user(request):
@@ -81,7 +101,7 @@ def account_address_add(request):
 
 @login_required
 def account_address_update(request, id):
-    if request.method == 'PUT':
+    if request.method == 'POST':
         form = AddressForm(request.PUT)
         if form.is_valid():
             # create new address
