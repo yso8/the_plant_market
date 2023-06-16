3a2330060b26a666f8206cc7624f19eb3ba06c41
louis <louis.alary34@gmail.com>
2023-06-16 17:03:49 +0200

    Removed unused elements

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Order, Cart, Delivery, Category
+from .models import Product, Order, Cart, Delivery, Category, CartProduct
 
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Cart)
 admin.site.register(Delivery)
 admin.site.register(Category)
+admin.site.register(CartProduct)
