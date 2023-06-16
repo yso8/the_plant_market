a4a89a7ff73a134f065c8bb5bbc462042f5e117c
louis <louis.alary34@gmail.com>
2023-06-16 17:02:23 +0200

    Add env file to add key for Stripe

--- shop/settings.py
+++ shop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +126,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = 'store.Shopper'
 
 LOGIN_URL='/login/'
+
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
